8021375e6f3b212988cde5806d2d72ee1b07b455
Danern <43783801+Danern@users.noreply.github.com>
2018-12-20 02:38:04 -0800

    Update pong.js

--- pong.js
+++ pong.js
@@ -1,9 +1,10 @@
 // select canvas element
 const canvas = document.getElementById("pong");
 
+
 // getContext of canvas = methods and properties to draw and do a lot of thing to the canvas
 const ctx = canvas.getContext('2d');
-
+context.strokeStyle = '#7CFC00';
 // load sounds
 let hit = new Audio();
 let wall = new Audio();
